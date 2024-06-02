853223406e2586a71617906981de05e78ec1ccd3
MrAmericanMike <mramericanmike@gmail.com>
2024-06-02 11:25:57 -0300

    Packages updated

--- utils/RateLimit.ts
+++ utils/RateLimit.ts
@@ -1,10 +1,10 @@
-const HISTORY = new Map();
+const HISTORY = new Map<string, number>();
 
 function rateLimit(ip: string, seconds: number): boolean {
 	if (HISTORY.has(ip)) {
 		const TIME_AGO = Date.now() - seconds * 1000;
 		const LAST_CALL = HISTORY.get(ip);
-		if (TIME_AGO < LAST_CALL) {
+		if (TIME_AGO < LAST_CALL!) {
 			return true;
 		}
 	}
